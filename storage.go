5cd39c914c068264be2ef6bf92a725980b2bc37f
Tanopwan Tangprakit <tanopwan@gmail.com>
2019-04-19 15:20:03 +0700

    initial commit

---
+++ storage.go
@@ -0,0 +1,150 @@
+package dupreq
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"os"
+	"reflect"
+	"time"
+)
+
+const prefix = "ref_so_"
+
+func GetNewRedisPool() *redis.Pool {
+	host := os.Getenv("REDIS_HOST")
+	log.Println("getting redis pool from ", host)
+	redisPool := &redis.Pool{
+		MaxIdle:     2,
+		IdleTimeout: 60 * time.Minute,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", host+":6379")
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) > time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+
+	return redisPool
+}
+
+type StorageService struct {
+	redis *redis.Pool
+}
+
+func NewStorageService(redisPool *redis.Pool) *StorageService {
+	if redisPool == nil {
+		redisPool = GetNewRedisPool()
+	}
+	return &StorageService{
+		redis: redisPool,
+	}
+}
+
+type Request struct {
+	Begin      time.Time
+	End        time.Time
+	StatusCode int
+	Body       []byte
+}
+
+func (r Request) ToGOB64() string {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	err := e.Encode(r)
+	if err != nil {
+		log.Println(`failed gob Encode`, err)
+	}
+	return base64.StdEncoding.EncodeToString(b.Bytes())
+}
+
+func (r *Request) FromGOB64(str string) {
+	by, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		log.Println(`failed base64 Decode`, err)
+	}
+	b := bytes.Buffer{}
+	b.Write(by)
+	d := gob.NewDecoder(&b)
+	err = d.Decode(&r)
+	if err != nil {
+		log.Println(`failed gob Decode`, err)
+	}
+}
+
+func (s *StorageService) SaveIncomingRequest(requestID string, request Request) error {
+	db := s.redis.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	reply, err := db.Do("SETNX", prefix+requestID, request.ToGOB64())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[SaveIncomingRequest] reply: %+v, %s\n", reply, reflect.TypeOf(reply))
+	r, ok := reply.(int64)
+	if !ok || r == 0 {
+		// already exists
+		return errors.New("key already exists")
+	}
+
+	return nil
+}
+
+func (s *StorageService) GetRequest(requestID string) (*Request, error) {
+	db := s.redis.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	reply, err := db.Do("GET", prefix+requestID)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := redis.String(reply, nil)
+	fmt.Printf("[GetRequest] reply: %s, %s\n", r, reflect.TypeOf(reply))
+	if err != nil {
+		return nil, errors.New("error parsing reply string: " + err.Error())
+	}
+
+	request := Request{}
+	request.FromGOB64(r)
+
+	return &request, nil
+}
+
+func (s *StorageService) SaveOutgoingRequest(requestID string, request Request) error {
+	db := s.redis.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	reply, err := db.Do("SETEX", prefix+requestID, int64(1*time.Hour/time.Second), request.ToGOB64())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[SaveOutgoingRequest] reply: %+v, %s\n", reply, reflect.TypeOf(reply))
+
+	return nil
+}
---
+++ storage_test.go
@@ -0,0 +1,164 @@
+package dupreq_test
+
+import (
+	. "github.com/tanopwan/dupreq"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestRequestGOB64(t *testing.T) {
+	request := Request{
+		Begin:      time.Time{},
+		End:        time.Time{},
+		StatusCode: http.StatusOK,
+		Body:       []byte("hello world"),
+	}
+
+	gob64 := request.ToGOB64()
+	t.Logf("GOB64: %s", gob64)
+
+	newRequest := Request{}
+	newRequest.FromGOB64(gob64)
+
+	if string(request.Body) != string(newRequest.Body) {
+		t.Errorf("invalid parsing")
+	}
+	t.Log(string(newRequest.Body))
+}
+
+func TestSaveAndGetRequest(t *testing.T) {
+	pool := GetNewRedisPool()
+	db := pool.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	_, err := db.Do("DEL", "ref_so_1234")
+	if err != nil {
+		t.Errorf("test fail to prepare test data: %s\n", err.Error())
+	}
+
+	request := Request{
+		Begin:      time.Now(),
+		End:        time.Now(),
+		StatusCode: http.StatusOK,
+		Body:       []byte("hello world"),
+	}
+
+	service := NewStorageService(pool)
+	err = service.SaveIncomingRequest("1234", request)
+	if err != nil {
+		t.Errorf("test fail to save incomming request: %s\n", err.Error())
+	}
+
+	getRequest, err := service.GetRequest("1234")
+	if err != nil {
+		t.Errorf("test fail to save get request: %s\n", err.Error())
+	}
+
+	t.Logf("getRequest: %+v\n", getRequest)
+	if request.Begin.String() == getRequest.Begin.String() || request.End.String() == getRequest.End.String() {
+		t.Errorf("test fail Begin/End")
+	}
+
+	if request.StatusCode != getRequest.StatusCode {
+		t.Errorf("test fail StatusCode")
+	}
+
+	if string(request.Body) != string(getRequest.Body) {
+		t.Errorf("test fail Body")
+	}
+}
+
+func TestSaveRequestDup(t *testing.T) {
+	pool := GetNewRedisPool()
+	db := pool.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	_, err := db.Do("SET", "ref_so_1234", "ex")
+	if err != nil {
+		t.Errorf("test fail to prepare test data: %s\n", err.Error())
+	}
+
+	request := Request{
+		Begin:      time.Time{},
+		End:        time.Time{},
+		StatusCode: http.StatusOK,
+		Body:       []byte("hello world"),
+	}
+
+	service := NewStorageService(pool)
+	err = service.SaveIncomingRequest("1234", request)
+	if err == nil {
+		t.Errorf("test fail to save incomming request: should be error\n")
+	}
+	t.Logf("expected error: %s\n", err.Error())
+}
+
+func TestSaveAndUpdateRequest(t *testing.T) {
+	pool := GetNewRedisPool()
+	db := pool.Get()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection after create session with reason: %s\n", err.Error())
+		}
+	}()
+
+	_, err := db.Do("DEL", "ref_so_1234")
+	if err != nil {
+		t.Errorf("test fail to prepare test data: %s\n", err.Error())
+	}
+
+	request := Request{
+		Begin:      time.Now(),
+		End:        time.Time{},
+		StatusCode: 0,
+		Body:       nil,
+	}
+
+	service := NewStorageService(pool)
+	err = service.SaveIncomingRequest("1234", request)
+	if err != nil {
+		t.Errorf("test fail to save incomming request: %s\n", err.Error())
+	}
+
+	time.Sleep(time.Second)
+
+	request.End = time.Now()
+	request.StatusCode = http.StatusOK
+	request.Body = []byte("hello world")
+
+	err = service.SaveOutgoingRequest("1234", request)
+	if err != nil {
+		t.Errorf("test fail to save request: %s\n", err.Error())
+	}
+
+	getRequest, err := service.GetRequest("1234")
+	if err != nil {
+		t.Errorf("test fail to get request: %s\n", err.Error())
+	}
+
+	t.Logf("getRequest: %+v\n", getRequest)
+	if request.Begin.String() == getRequest.Begin.String() || request.End.String() == getRequest.End.String() {
+		t.Errorf("test fail Begin/End")
+	}
+
+	if 200 != getRequest.StatusCode {
+		t.Errorf("test fail StatusCode")
+	}
+
+	if "hello world" != string(getRequest.Body) {
+		t.Errorf("test fail Body")
+	}
+}
